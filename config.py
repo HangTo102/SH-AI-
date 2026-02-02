897c329c2108625d5ea7f6e85cdb2be283d7d99e
Hang_Ti_ <hantaosama@qq.com>
2026-02-02 10:55:14 +0800

    Reorganize imports and uncomment USE_AI variable

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 import os
+import streamlit as st
 
 # USE_AI = os.getenv("USE_AI", "false").lower() == "true"      # 仅适用于本地环境变量
 
 USE_AI = st.secrets.get("USE_AI", "false").lower() == "true"     # 适用于 streamlit 平台使用
+
